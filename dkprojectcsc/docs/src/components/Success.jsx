72e3993e3501006fa095b518b1e87c23cf4f58aa
Akshay <akshaysabharwal92@gmail.com>
2024-08-17 17:47:11 +0530

    initial commit

---
+++ dkprojectcsc/docs/src/components/Success.jsx
@@ -0,0 +1,14 @@
+
+const Success = () => {
+    return (
+        <div className=" ">
+            <div className="">
+                <h1>Successful!</h1>
+                <p>Your Mobile number has been successfully verified</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Success
