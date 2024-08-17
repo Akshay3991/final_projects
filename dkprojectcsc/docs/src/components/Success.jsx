7822eebd341a9160187e5aab99c88dbd1bd95e30
Akshay <akshaysabharwal92@gmail.com>
2024-08-17 18:14:50 +0530

    made success page

--- dkprojectcsc/docs/src/components/Success.jsx
+++ dkprojectcsc/docs/src/components/Success.jsx
@@ -1,7 +1,7 @@
 
 const Success = () => {
     return (
-        <div className="bg-[red]">
+        <div className="">
             <div className="">
                 <h1>Successful!</h1>
                 <p>Your Mobile number has been successfully verified</p>
