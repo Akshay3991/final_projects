63ace88ad98b0cf866e9792136a17329686cdea5
Akshay <akshaysabharwal92@gmail.com>
2024-08-17 12:16:42 +0530

    import errors

--- dkproject/docs/src/main.jsx
+++ dkproject/docs/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/App.jsx'
+import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
