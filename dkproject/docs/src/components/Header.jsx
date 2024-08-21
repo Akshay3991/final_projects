576c0de975253ab51258febb007da942dcc627ba
Akshay <akshaysabharwal92@mail.com>
2024-08-21 17:10:54 +0530

    added routers to header

--- dkproject/docs/src/components/Header.jsx
+++ dkproject/docs/src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            Header
+           <header>
+            <nav>
+                <ul>
+                   <Link to='/'>
+                   <li>Home</li>
+                   </Link> 
+                   <Link to='/orders'>
+                   <li>Orders</li>
+                   </Link> 
+                   <Link to='/cart'>
+                   <li>Cart</li>
+                   </Link> 
+                    
+                </ul>
+            </nav>
+           </header>
         </div>
     )
 }
