a68ba9afcd98e92dd39abc4e3cde85bc45719a73
Akshay <akshaysabharwal92@mail.com>
2024-08-22 17:12:38 +0530

    importing css

--- dkproject/docs/src/main.jsx
+++ dkproject/docs/src/main.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './routes/App.jsx'
-import Home from './components/Home.jsx'
-import Cart from './components/Cart.jsx'
-import Otp from './components/Otp.jsx'
-import Orders from './components/Orders.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./routes/App.jsx";
+import Home from "./routes/Home.jsx";
+import Cart from "./routes/Cart.jsx";
+import Otp from "./components/Otp.jsx";
+import Orders from "./components/Orders.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: "/", element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: "/cart", element: <Cart />
+        path: "/cart",
+        element: <Cart />,
       },
       {
-        path: "/otp", element: <Otp />
+        path: "/otp",
+        element: <Otp />,
       },
       {
-        path: "/orders", element: <Orders />
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
