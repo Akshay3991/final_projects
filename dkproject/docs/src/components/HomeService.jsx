6afbecb9b6d3baaa46a514235b9c500e0e30525c
Akshay <akshaysabharwal92@mail.com>
2024-08-22 17:16:05 +0530

    added services to home page

---
+++ dkproject/docs/src/components/HomeService.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../store/cartSlice";
+import { GrAddCircle } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
+
+const HomeService = ({ service }) => {
+  const dispatch = useDispatch();
+  const cartServices = useSelector((store) => store.cart);
+  const elementFound = cartServices.indexof(service.id) >= 0;
+
+  const handleAddToCart = () => {
+    dispatch(cartActions.addToCart(service.id));
+  };
+  const handleRemove = () => {
+    dispatch(cartActions.removeFromCart(service.id));
+  };
+
+  return (
+    <div>
+      {/* design */}
+
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-danger"
+          onClick={handleRemove}
+        >
+          <AiFillDelete /> Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-success"
+          onClick={handleAddToCart}
+        >
+          <GrAddCircle /> Add to Bag
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default HomeService;
