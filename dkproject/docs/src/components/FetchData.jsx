a5013bb9b14e9a1a287882632c10a56f6687d23b
Akshay <akshaysabharwal92@mail.com>
2024-08-23 10:51:11 +0530

    fetching api

---
+++ dkproject/docs/src/components/FetchData.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { servicesActions } from "../store/serviceSlice";
+import { fetchStatusActions } from "../store/fetchStatusSlice";
+
+function FetchData() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fetchStatus.fetchDone) return;
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+    dispatch(fetchStatusActions.markFetchingStarted());
+    fetch("http://localhost:8080/services", { signal })
+      .then((res) => res.json())
+      .then(({ services }) => {
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingStarted());
+        dispatch(servicesActions.addInitialServices(services));
+      });
+    return () => {
+      controller.abort();
+    };
+  }, [fetchStatus]);
+  return <div></div>;
+}
+
+export default FetchData;
