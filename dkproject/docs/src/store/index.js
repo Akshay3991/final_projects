7059f7cd17abe3c593548dc0051533e052f005a6
Akshay <akshaysabharwal92@mail.com>
2024-08-23 10:52:00 +0530

    added fetching state in redux store

--- dkproject/docs/src/store/index.js
+++ dkproject/docs/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import serviceSlice from "./serviceSlice";
 import cartSlice from "./cartSlice";
 import formSlice from "./formSlice";
+import fetchStatusSlice from "./fetchStatusSlice";
 
 const dataStore = configureStore({
   reducer: {
     services: serviceSlice.reducer,
     cart: cartSlice.reducer,
     form: formSlice.reducer,
+    fetchStatus: fetchStatusSlice.reducer,
   },
 });
 
