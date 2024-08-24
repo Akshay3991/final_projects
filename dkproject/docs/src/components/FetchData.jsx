c64e6f39a71d4dc39b546a316c09be9a456c3424
Akshay <akshaysabharwal92@mail.com>
2024-08-24 19:12:01 +0530

    added import statement

--- dkproject/docs/src/components/FetchData.jsx
+++ dkproject/docs/src/components/FetchData.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { servicesActions } from "../store/serviceSlice";
 import { fetchStatusActions } from "../store/fetchStatusSlice";
+import { useEffect } from "react";
 
 function FetchData() {
   const fetchStatus = useSelector((store) => store.fetchStatus);
