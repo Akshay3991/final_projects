1dca6d1524ecf5d0c2defe98966cdbfa6b7fe043
Akshay <akshaysabharwal92@mail.com>
2024-08-24 19:12:22 +0530

    added footer logic

--- dkproject/docs/src/components/Footer.jsx
+++ dkproject/docs/src/components/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <footer className="absolute bottom-0">
+      <div className="bg-[#2a2626] text-center w-[100vw] h-[10vh] ">
+        <h5 className="text-[white] font-serif">Footer</h5>
+        <hr className="text-[white]" />
+        <div className="text-[whitesmoke]  font-serif font-semibold">
+          <h5 className="">2024@ All Rights Reserved</h5>
         </div>
-    )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
