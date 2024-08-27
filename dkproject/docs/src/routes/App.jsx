87f1fffa9c521dcce762922c94d26169551c9211
Akshay <akshaysabharwal92@mail.com>
2024-08-27 14:51:12 +0530

    added fetchstatus

--- dkproject/docs/src/routes/App.jsx
+++ dkproject/docs/src/routes/App.jsx
@@ -1,14 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import FetchData from "../components/FetchData";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function App() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
   return (
-    <div className="">
+    <>
       <Header />
-      <Outlet />
+      <FetchData />
+      {fetchStatus.currentlyFetching ? <LoadingSpinner /> : <Outlet />}
       <Footer />
-    </div>
+    </>
   );
 }
 
