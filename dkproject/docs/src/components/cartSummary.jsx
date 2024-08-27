416b6abcd2affc0e45ef42519bb57f7dfc68e0a4
Akshay <akshaysabharwal92@mail.com>
2024-08-27 16:13:39 +0530

    price summary added

--- dkproject/docs/src/components/cartSummary.jsx
+++ dkproject/docs/src/components/cartSummary.jsx
@@ -1,14 +1,30 @@
-import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const cartSummary = () => {
+const CartSummary = () => {
+  const navigate = useNavigate();
+  const clickHandler = () => {
+    navigate("/form");
+  };
+  let price = 0;
   const cartServiceIds = useSelector((state) => state.cart);
   const services = useSelector((state) => state.services);
   const finalServices = services.filter((service) => {
-    const serviceIndex = cartServiceIds.indexof(service.id);
+    const serviceIndex = cartServiceIds.indexOf(service.id);
     return serviceIndex >= 0;
   });
-  return <div>cartSummary</div>;
+  finalServices.forEach((service) => {
+    price += service.original_price;
+  });
+  return (
+    <div>
+      <h1>Total price of {finalServices.length} services:</h1>
+      <span>$ {price}</span>
+      <div>
+        <button onClick={clickHandler}>Place Order</button>
+      </div>
+    </div>
+  );
 };
 
-export default cartSummary;
+export default CartSummary;
