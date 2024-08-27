1bbea78e45a1625fa9f2fcb0fc632da44c0fb15b
Akshay <akshaysabharwal92@mail.com>
2024-08-27 16:13:19 +0530

    services IDs added

--- dkproject/docs/src/components/cartService.jsx
+++ dkproject/docs/src/components/cartService.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { RiDeleteBin5Fill } from "react-icons/ri";
 import { useDispatch } from "react-redux";
+import { RiDeleteBin5Fill } from "react-icons/ri";
 import { cartActions } from "../store/cartSlice";
 
-const cartService = ({ service }) => {
+const CartService = ({ service }) => {
   const dispatch = useDispatch();
+
   const handleRemoveService = () => {
     dispatch(cartActions.removeFromCart(service.id));
   };
   return (
     <main>
-      <div className="" onClick={handleRemoveItem}>
+      <div className="" onClick={handleRemoveService}>
         <RiDeleteBin5Fill />
       </div>
     </main>
   );
 };
 
-export default cartService;
+export default CartService;
